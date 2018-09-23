f05ecde178eec01ec869dbc529d5c04ca91f40d7
intranetexplorer <pavan01@gmail.com>
2018-09-23 15:14:06 -0500

    removed while

--- distancetime.py
+++ distancetime.py
@@ -1,3 +1,4 @@
+#!/usr/bin/env python2
 #42.057562,-88.041430 - 1600 mcconnor pkwy schaumburg
 #41.872293,-87.633294  - 170 W Polk Chicago
 # https://maps.googleapis.com/maps/api/distancematrix/json?origins=42.057562,-88.041430&destinations=41.872293,-87.633294&departure_time=now&units=imperial&key=AIzaSyCVvUXJuWMS5ap0XC8oQK0XNaKFq3XtFXw
@@ -18,26 +19,26 @@ import time;
 import json;
 import requests;
 
-i=1
-while i>0:
+#i=1
+#while i>0:
 	#t=datetime.datetime.now()
 	#print(str(t)+','+ "\n")
-	req=requests.get('https://maps.googleapis.com/maps/api/distancematrix/json?origins=41.872293,-87.633294&destinations=42.057562,-88.041430&departure_time=now&units=imperial&key=AIzaSyCVvUXJuWMS5ap0XC8oQK0XNaKFq3XtFXw')
-	resp=req.json()
-	duration_text=resp['rows'][0]['elements'][0]['duration_in_traffic']['text']
-	duration_secs=resp['rows'][0]['elements'][0]['duration_in_traffic']['value']
-	C2S=str(datetime.datetime.now())+'|'+'CHI2SCH'+'|'+str(duration_text)+ '|' + str(duration_secs)
-	#print C2S
-	req2=requests.get('https://maps.googleapis.com/maps/api/distancematrix/json?origins=42.057562,-88.041430&destinations=41.872293,-87.633294&departure_time=now&units=imperial&key=AIzaSyCVvUXJuWMS5ap0XC8oQK0XNaKFq3XtFXw')
-	resp2=req2.json()
-	duration_text2=resp2['rows'][0]['elements'][0]['duration_in_traffic']['text']
-	duration_secs2=resp2['rows'][0]['elements'][0]['duration_in_traffic']['value']
-	S2C=str(datetime.datetime.now())+'|'+'SCH2CHI'+'|'+str(duration_text2)+ '|' + str(duration_secs2)
-	#print S2C
-	f = open("traveltime", "a")
-	f.write(C2S+'\n'+S2C +'\n')
-	f.close()
-	time.sleep(300)
-	i+=1
+req=requests.get('https://maps.googleapis.com/maps/api/distancematrix/json?origins=41.872293,-87.633294&destinations=42.057562,-88.041430&departure_time=now&units=imperial&key=AIzaSyCVvUXJuWMS5ap0XC8oQK0XNaKFq3XtFXw')
+resp=req.json()
+duration_text=resp['rows'][0]['elements'][0]['duration_in_traffic']['text']
+duration_secs=resp['rows'][0]['elements'][0]['duration_in_traffic']['value']
+C2S=str(datetime.datetime.now())+'|'+'CHI2SCH'+'|'+str(duration_text)+ '|' + str(duration_secs)
+#print C2S
+req2=requests.get('https://maps.googleapis.com/maps/api/distancematrix/json?origins=42.057562,-88.041430&destinations=41.872293,-87.633294&departure_time=now&units=imperial&key=AIzaSyCVvUXJuWMS5ap0XC8oQK0XNaKFq3XtFXw')
+resp2=req2.json()
+duration_text2=resp2['rows'][0]['elements'][0]['duration_in_traffic']['text']
+duration_secs2=resp2['rows'][0]['elements'][0]['duration_in_traffic']['value']
+S2C=str(datetime.datetime.now())+'|'+'SCH2CHI'+'|'+str(duration_text2)+ '|' + str(duration_secs2)
+#print S2C
+f = open("traveltime", "a")
+f.write(C2S+'\n'+S2C +'\n')
+f.close()
+#	time.sleep(300)
+#	i+=1
 
 
